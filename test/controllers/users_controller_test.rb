7f73e184fec1a118b7940e0f38827ea994a24d5e
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-09 04:29:40 +0100

    Fixe users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = User.new(username: "user", email: "user@example.com", password: 'secure_password', password_confirmation: 'secure_password')
+    @newton = users(:newton)
   end
 
   test "should get index" do
@@ -12,25 +13,25 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password: 'secret', password_confirmation: 'secret', username: @user.username } }, as: :json
+      post users_url, params: { user: { email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation, username: @user.username } }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show user" do
-    get user_url(@user), as: :json
+    get user_url(@newton), as: :json
     assert_response :success
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password: 'secret', password_confirmation: 'secret', username: @user.username } }, as: :json
+    patch user_url(@newton), params: { user: { email: "newton@example.com", password: 'appleinthehead', password_confirmation: 'appleinthehead', username: 'isaac.newton' } }, as: :json
     assert_response 200
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
+      delete user_url(@newton), as: :json
     end
 
     assert_response 204
