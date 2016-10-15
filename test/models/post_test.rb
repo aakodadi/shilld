1f8505f233a5996e0a7a8f178b6a11d5097de524
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-15 05:00:02 +0100

    Add posts model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.new(body: "This is a test post")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "body must be" do
+    @post.body = nil
+    assert_not @post.valid?
+    assert_equal ["can't be blank"],
+      @post.errors.messages[:body]
+  end
+
+  test "too long body" do
+    @post.body = "a" * 5001
+    assert_not @post.valid?
+    assert_equal ["is too long (maximum is 5000 characters)"],
+      @post.errors.messages[:body]
+  end
 end
