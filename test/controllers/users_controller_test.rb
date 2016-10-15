3b2c66b1c21f5e81daae132f64a8f02f3e6ddaa6
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-15 04:44:59 +0100

    Add controler test for unvalid user creation and update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,10 +13,32 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, username: @user.username, name: @user.name, password: @user.password, password_confirmation: @user.password_confirmation } }, as: :json
+      post users_url, params: {
+        user: { email: @user.email,
+          username: @user.username,
+          name: @user.name, password:
+          @user.password,
+          password_confirmation: @user.password_confirmation
+        }
+      }, as: :json
     end
 
     assert_response 201
+  end
+
+  test "should not create user if unvalid" do
+    assert_no_difference('User.count') do
+      post users_url, params: {
+        user: { email: @user.email,
+          username: @user.username,
+          name: @user.name, password:
+          @user.password,
+          password_confirmation: "password"
+        }
+      }, as: :json
+    end
+
+    assert_response 422
   end
 
   test "should show user" do
@@ -25,8 +47,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@newton.username), params: { user: { email: "newton@example.com", name: "Newton", password: 'appleinthehead', password_confirmation: 'appleinthehead', username: 'isaac.newton' } }, as: :json
+    patch user_url(@newton.username), params: {
+      user: { email: "newton@example.com",
+        name: "Newton",
+        password: 'appleinthehead',
+        password_confirmation: 'appleinthehead',
+        username: 'isaac.newton'
+      }
+    }, as: :json
     assert_response 200
+  end
+
+  test "should not update user if unvalid" do
+    patch user_url(@newton.username), params: {
+      user: { email: "newton@example.com",
+        name: "Newton",
+        password: 'appleinthehead',
+        password_confirmation: 'password',
+        username: 'isaac.newton'
+      }
+    }, as: :json
+    assert_response 422
   end
 
   test "should destroy user" do
