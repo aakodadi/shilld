eec5789450b1cb02781e4a36601001d476024915
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-15 05:27:48 +0100

    Add created and updated at params to users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :name, :email
+  attributes :id, :username, :name, :email, :created_at, :updated_at
+
+  def created_at
+    object.created_at.to_i
+  end
+
+  def updated_at
+    object.updated_at.to_i
+  end
 end
