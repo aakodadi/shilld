e528d345a22967d154695e6e14913d74dd11e69f
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-15 03:38:50 +0100

    Add username format validation (only letters, digits, ., _, and - are permited)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9.\-_]+\Z/
 
   has_secure_password
 
@@ -8,8 +9,10 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :username, presence: true, length: { maximum: 50 },
+    format: { with: VALID_USERNAME_REGEX },
     uniqueness: { case_sensitive: false }
   validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
 end
