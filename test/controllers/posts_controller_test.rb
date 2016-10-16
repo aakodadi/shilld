131086734542a199bd6ac9ff61444a6128f28253
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-16 09:55:56 +0100

    Add posts controller tests for authorizations

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -6,6 +6,11 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     login_as(User.new(username: 'newton', password: 'appleinthehead'))
   end
 
+  test "should not get index if not authenticated" do
+    get posts_url, as: :json
+    assert_response :unauthorized
+  end
+
   test "should get index" do
     get posts_url, headers: get_headers, as: :json
     assert_response :success
@@ -17,6 +22,14 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_response 201
+  end
+
+  test "should not create post if not authenticated" do
+    assert_no_difference('Post.count') do
+      post posts_url, params: { post: { body: @post.body } }, as: :json
+    end
+
+    assert_response :unauthorized
   end
 
   test "should not create post if unvalid" do
@@ -32,9 +45,25 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should not show post if not authenticated" do
+    get post_url(@post), as: :json
+    assert_response :unauthorized
+  end
+
   test "should update post" do
     patch post_url(@post), headers: get_headers, params: { post: { body: @post.body } }, as: :json
     assert_response 200
+  end
+
+  test "should not update post if not authenticated" do
+    patch post_url(@post), params: { post: { body: @post.body } }, as: :json
+    assert_response :unauthorized
+  end
+
+  test "should not update post if not authenticated as author" do
+    @post = posts(:two)
+    patch post_url(@post), headers: get_headers, params: { post: { body: @post.body } }, as: :json
+    assert_response :unauthorized
   end
 
   test "should not update post if unvalid" do
@@ -48,5 +77,22 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_response 204
+  end
+
+  test "should not destroy post if not authenticated" do
+    assert_no_difference('Post.count') do
+      delete post_url(@post), as: :json
+    end
+
+    assert_response :unauthorized
+  end
+
+  test "should not destroy post if not authenticated as author" do
+    @post = posts(:two)
+    assert_no_difference('Post.count') do
+      delete post_url(@post), headers: get_headers, as: :json
+    end
+
+    assert_response :unauthorized
   end
 end
