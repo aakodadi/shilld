c49d9c692f2539d5cc588e0385237a9c80e6b84e
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-16 12:02:35 +0100

    Add more auths controller tests

--- test/controllers/auths_controller_test.rb
+++ test/controllers/auths_controller_test.rb
@@ -6,7 +6,7 @@ class AuthsControllerTest < ActionDispatch::IntegrationTest
     @newton = users(:newton)
   end
 
-  test "should authenticate and unauthenticate" do
+  test "should authenticate and unauthenticate with username" do
     post auth_url,
       params: { user: {
           username: @newton.username,
@@ -21,6 +21,53 @@ class AuthsControllerTest < ActionDispatch::IntegrationTest
         'Authorization' => %(Token token="#{user["auth_token"]}", username="#{@newton.username}")
       }
     assert_response :success
+  end
+
+  test "should authenticate and unauthenticate with email" do
+    post auth_url,
+      params: { user: {
+          email: @newton.email,
+          password: 'appleinthehead'
+        }
+      }, as: :json
+    assert_response :success
+    user = JSON.parse(@response.body)
+    assert_equal @newton.username, user["username"]
+    delete auth_url,
+      headers: {
+        'Authorization' => %(Token token="#{user["auth_token"]}", username="#{@newton.username}")
+      }
+    assert_response :success
+  end
+
+  test "should not authenticate with wrong password" do
+    post auth_url,
+      params: { user: {
+          username: @newton.username,
+          password: 'newton'
+        }
+      }, as: :json
+    assert_response :unauthorized
+  end
+
+  test "should not authenticate with wrong username" do
+    post auth_url,
+      params: { user: {
+          username: "newtonn",
+          password: 'appleinthehead'
+        }
+      }, as: :json
+    assert_response :unprocessable_entity
+  end
+
+  test "should not authenticate with wrong email" do
+    post auth_url,
+      params: { user: {
+          eamil: "newtonn@example.com",
+          password: 'appleinthehead'
+        }
+      }, as: :json
+    assert_response :unprocessable_entity
   end
 
 end
