2f10eff2ef5c5bf48c00605e8a723276f9010292
Abdelhakim Akodadi <akodadi.abdelhakim@gmail.com>
2016-10-29 00:53:54 +0100

    Add user association in post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :body, :created_at, :updated_at
+  belongs_to :user, serializer: UserSerializer
 
     def created_at
       object.created_at.to_i
